c85bef9109f2ea35558c06246102e63fd9032cec
laijihua <1164258202@qq.com>
2021-11-02 00:16:31 +0800

    添加register 和 login 接口4

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -3,21 +3,17 @@ import Leaf
 
 func routes(_ app: Application) throws {
     app.get { req in
-        return "It works!"
-    }
-
-    app.get("hello") { req  in
-        return req.view.render("hello", ["name": "Leaf"])
-    }
-
-    app.get("login") { req in 
-        return req.view.render("login", ["name": "Login"])
-    }
-
-    app.get("register") { req in 
-        return req.view.render("register", ["name": "Register"])
+        return req.view.render("index")
     }
     
+    app.group("auth") { auth in
+        auth.get("login") { req in
+            return req.view.render("auth/login", ["name": "Login"])
+        }
+        auth.get("register") { req in
+            return req.view.render("auth/register", ["name": "Register"])
+        }
+    }
     
     try app.group("api") { api in
         try api.register(collection: AuthController())
